e2688c58dc34126f177ec07660c18558c60d2f9e
Jacky Ngo <jacky.ngo96@gmail.com>
2022-09-22 07:53:50 -0400

    retrun pass or fail message depending on whether the arguments are equal

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,13 @@
-const assertEqual = function(actual, expected) { 
-if (actual === expected){ 
-  console.log(`🛑🛑🛑 Assertion Passed:${actual} === ${expected}`);
-} else {
-  console.log(`✅✅✅ Assertion Failed:${actual} !== ${expected}`); 
-}
-}; 
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  }
+};
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1); 
-assertEqual("apples","apples");
-assertEqual("apples","oranges");
-assertEqual(5,5); 
-assertEqual(5,9); 
+assertEqual(1, 1);
+assertEqual("apples", "apples");
+assertEqual("apples", "oranges");
+assertEqual(5, 5);
+assertEqual(5, 9);
