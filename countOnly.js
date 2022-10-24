b35a922e5b737f1250854a07a5cadccefb91525c
Jacky Ngo <jacky.ngo96@gmail.com>
2022-10-24 11:40:33 -0400

    Count names inside array only if condition is true

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+// report back how many instances of each string were found in the allItems array
+const countOnly = function (allItems, itemsToCount) {
+  // create empty object to gather results
+  const results = {};
+  // loop through the first names
+  for (const item of allItems) {
+    // if statement to increment results only if items to count are true
+    if (itemsToCount[item]) {
+      // increment the count of each item into results as we encounter each string item in the array
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
